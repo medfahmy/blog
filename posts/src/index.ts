3ebd8b9da8efaddf6435e780e8297bcf9f6c5328
medfahmy <medfahmy99@gmail.com>
2021-09-24 00:42:18 +0100

    initial commit

---
+++ posts/src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import { randomBytes } from "crypto";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+interface Post {
+    id: string;
+    content: string;
+}
+
+const posts: { [id: string]: Post } = {};
+
+const generateRandomId = () => {
+    return randomBytes(4).toString("hex");
+};
+
+app.get("/posts/get", (_req, res) => {
+    return res.json(posts);
+});
+
+app.post("/posts/post", (req, res) => {
+    console.log(req.body);
+
+    const id = generateRandomId();
+    const { content } = req.body;
+
+    posts[id] = {
+        id,
+        content,
+    };
+
+    return res.status(201).json(posts[id]);
+});
+
+app.listen(4000, () => {
+    console.log("server running at http://localhost:4000");
+});
